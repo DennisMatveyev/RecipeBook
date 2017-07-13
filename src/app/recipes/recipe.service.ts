ca55c4bcd8fbe0b1b53686e05b53fd0cd768a109
Dennis <d.matveev@bigmir.net>
2017-07-13 16:57:41 +0300

    commit

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,11 @@
+import {EventEmitter} from "@angular/core";
+
 import {Recipe} from "./recipe.model";
 
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('Test recipe 1', 'It is test description 1',
                'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from "./recipe.model";
-import {RecipeService} from "./recipe.service";
+import { RecipeService } from "./recipe.service";
 
 
 @Component({
@@ -12,9 +12,14 @@ import {RecipeService} from "./recipe.service";
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe: Recipe) => {
+        this.selectedRecipe = recipe;
+      }
+    );
   }
 
 }
