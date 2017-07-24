0696ab39fe2a0257b62779bded58eac7d43a86f5
DennisMatveyev <d.matveev@bigmir.net>
2017-07-24 10:57:09 +0300

    commit

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -7,7 +7,7 @@ import { RecipeService } from "./recipe.service";
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService]
+  // providers: [RecipeService] // removed to app.module.ts
 })
 export class RecipesComponent implements OnInit {
   // selectedRecipe: Recipe;
