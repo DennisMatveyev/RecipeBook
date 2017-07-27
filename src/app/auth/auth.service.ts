61c8565680bf0a52f58e154f5e15d32b5dbfbed3
DennisMatveyev <d.matveev@bigmir.net>
2017-07-27 12:26:50 +0300

    commit

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,10 @@
+// Authentication backend is Firebase
+export class AuthService {
+  signupUser(email: string, password: string) {
+    // here Firebase API goes
+  }
+
+  signinUser(email: string, password: string) {
+    // here Firebase API goes
+  }
+}
